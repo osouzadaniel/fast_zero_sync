c845bc84b6476af1fcf44869761cd145d3e4d447
Daniel Souza <souzasc@gmail.com>
2024-07-09 15:06:27 -0300

    Adicionado routs CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,95 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
-
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testusername',
+            'password': 'password',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'testusername',
+        'id': 1,
+        'email': 'test@test.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'testusername',
+                'id': 1,
+                'email': 'test@test.com',
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'testusername2',
+            'password': 'password',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.json() == {
+        'username': 'testusername2',
+        'id': 1,
+        'email': 'test@test.com',
+    }
+
+
+def test_update_invalid_user(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'testusername2',
+            'password': 'password',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_get_user_with_id(client):
+    response = client.get('/users/1')
+
+    assert response.json()['id'] == 1
+
+
+def test_get_invalid_user_with_id(client):
+    response = client.get('/users/121')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_invalid_user(client):
+    response = client.delete('/users/2')
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
